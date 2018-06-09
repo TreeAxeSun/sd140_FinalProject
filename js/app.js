b0bdbeb74e65c69d0f9ca7b6aca76e58b1d53298
Tree-Axe-Sun <veris1975@outlook.com>
2018-06-09 16:48:08 -0500

    Edit index.html and app.js

--- js/app.js
+++ js/app.js
@@ -1,13 +1,3 @@
-/*if ('serviceWorker' in navigator) {
-    window.addEventListener('load', function () {
-      navigator.serviceWorker.register('serviceWorker.js').then(function (registration) {
-        console.log('ServiceWorker registration successful with scope: ', registration.scope);
-      }, function (error) {
-        console.log('ServiceWorker registration failed: ', error);
-      });
-    });
-  }*/
-
   (function(){
       if(!('serviceWorker' in navigator)){
           console.log('Service worker not supported');
